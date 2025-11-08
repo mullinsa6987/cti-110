0e746cd5013363300fd9da4d89fa8889b4acaf45
mullinsa6987 <mullinsa6987@student.faytechcc.edu>
2025-11-08 00:06:01 -0500

    First Python assignment
    taking input from user and printing output

---
+++ P1LAB1_mullins.py
@@ -0,0 +1,7 @@
+#Alexandria Mullins
+#p1lab1.py
+#input and print
+
+first_name = input("enter your first name: ")
+last_name = input ("enter your last name:")
+print("Hello,",first_name,last_name +"!","Welcome to CTI110!")
